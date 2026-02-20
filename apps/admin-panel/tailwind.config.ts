6c79a0ab90fbd406147801ed61f3b7270301f626
tienplt <phanletrongtien@gmail.com>
2026-02-20 18:55:36 +0700

    feat: Config lazy git

--- apps/admin-panel/tailwind.config.ts
+++ apps/admin-panel/tailwind.config.ts
@@ -2,7 +2,7 @@ import { createTailwindConfig } from "@libs/theme-token";
 import type { Config } from "tailwindcss";
 
 const config: Config = createTailwindConfig({
-  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
 });
 
 export default config;
